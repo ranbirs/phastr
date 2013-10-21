1c31502a7219d5e8f77d263fa54ed533933febc4
5rc <serge@5rc.org>
2013-10-21 22:52:51 +0100

    v0.5.1

--- sys/model.class.php
+++ sys/model.class.php
@@ -10,17 +10,13 @@ abstract class Model {
 
 	function __construct()
 	{
-		if (!isset($this->dbh)) {
-			
-			//$this->dbh = new Database($type . ":host=" . $host . ";dbname=" . $name, $user, $pass);
-		}
+
 	}
 
 	public function database($type = Database::type__, $host = Database::host__, $name = Database::name__, $user = Database::user__, $pass = Database::pass__)
 	{
-		if (!isset($this->dbh)) {
+		if (!isset($this->dbh))
 			$this->dbh = new Database($type . ":host=" . $host . ";dbname=" . $name, $user, $pass);
-		}
 		return $this->dbh;
 	}
 
