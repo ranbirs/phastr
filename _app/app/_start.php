0e853d925dfee78f803badbbc9ee297984867603
seht <seht@users.noreply.github.com>
2017-02-19 22:22:17 +0000

    sys/_bootstrap

--- _app/app/_start.php
+++ _app/app/_start.php
@@ -1,10 +1,4 @@
 <?php
-spl_autoload_register();
-
-$_error = new \sys\handlers\Error();
-
-set_error_handler([$_error, 'error']);
-set_exception_handler([$_error, 'exception']);
 
 $_routes = [
 	'index' => 'app/controllers/index', 
